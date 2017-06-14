6773fbebc52aba7f0acd2fd7357d037c974a882d
heber zelaya <berledid_3000@hotmail.com>
2017-06-15 01:39:13 +0200

    logeo con el facebook primera parte

--- client/login/loginform.js
+++ client/login/loginform.js
@@ -1,3 +1,60 @@
+Template.loginform.onRendered(function(){
+  self = this;
+  this.datos =new ReactiveVar();
+	$.getScript("//connect.facebook.net/en_US/sdk.js", function(){
+       BUTTONFACEBOOK.set(true);
+
+    	 window.fbAsyncInit = function() {
+           FB.init({
+               appId      : '113351262603077',
+              cookie     : true,
+               xfbml      : true,
+              version    : 'v2.8'
+          });
+
+                 FB.AppEvents.logPageView(); 
+                 // estamos verificando el etatus de facebook
+                 //checkLoggin();
+                /* FB.login(function(r){
+                 	console.log("->")
+                 	console.log(r);
+                 	testAPI();
+                 },{scope: 'email'});*/
+
+                 };
+                  function testAPI() {
+                      console.log('Welcome!  Fetching your information.... ');
+                      var datosfacebook = [];
+                      FB.api('/me?fields=email', function(response) {
+                         datosfacebook.push(response);
+                        FB.api("/me",function(r){
+                           datosfacebook.push(r);
+                          FB.api("/me/picture",function(r){
+                            datosfacebook.push(r);
+                            self.datos.set(datosfacebook);
+                            console.log(datosfacebook);
+
+                          });
+                        });
+                      	
+                       //console.log('Successful login for: ' + response.name);
+                     //console.log(response);
+               });
+  
+             };
+             checkLoggin =()=>{
+             	FB.getLoginStatus(function(response) {
+             			if(response.status == "connected"){
+                      		testAPI();
+                      	}
+                 	console.log(response);
+                 //statusChangeCallback(response);
+
+             
+         });
+        }
+     });
+});
 Template.loginform.events({
 	"click #regis" : function(e){
 		e.preventDefault();
--- client/paginaprincipal.js
+++ client/paginaprincipal.js
@@ -1,9 +1,14 @@
+BUTTONFACEBOOK =new ReactiveVar(false);
 Template.paginaprincipal.onRendered(function(){
 	$('.button-collapse').sideNav();
     $('.parallax').parallax();
     $(".panelForm").css("opacity",0);
+    
 });
 Template.paginaprincipal.helpers({
+	facebook: function(){
+		 return BUTTONFACEBOOK.get();
+	},
 	username : function(){
 		return Accounts.user().username;
 	}
