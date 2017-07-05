405aa0744ac492d8a216658771d843d904d533b2
heber zelaya <berledid_3000@hotmail.com>
2017-07-05 16:51:33 +0200

    modificando algunas fallas de logeo

--- client/newlogin/login.js
+++ client/newlogin/login.js
@@ -10,7 +10,47 @@ Template.newlogin.events({
 		}
 		Meteor.loginWithPassword(email,username);
 		console.log(email);
+		FlowRouter.go('/');
 		return false;
-	}
-			
+	}		
 })
+Template.newlogin.events({
+	"click #REGISTER" : function(e){
+		var username= document.getElementById("username");
+		var nombre= document.getElementById("nombre");
+		var apellido= document.getElementById("apellido");
+		var carrera= document.getElementById("carrera");
+		var email= document.getElementById("email");
+		var password= document.getElementById("password");
+		if( carrera!=undefined && password!=undefined && 
+			nombre!=undefined && apellido!=undefined && 
+			email!=undefined && username!=undefined )
+		{
+			email=email.value;
+			nombre=nombre.value;
+			password=password.value;
+			apellido=apellido.value;
+			carrera=carrera.value;
+			username=username.value;
+		}
+		var user = {
+			"username" : username,
+			"email" : email,
+			"password" : password,
+			"profile" : {
+				"Nombre" : nombre,
+				"Apellido" : apellido,
+				"Idioma" : carrera,
+				"estado" :false
+			   }
+	    };
+	    Accounts.createUser(user, function(e){
+				if(e == undefined) {								
+					Meteor.loginWithPassword(user.username,user.password);	
+				}
+		});
+		FlowRouter.go('/');
+		return false;
+	}		
+})
+
